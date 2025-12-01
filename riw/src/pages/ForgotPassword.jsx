e4e60a8b5cc3de2458d10ffa41ee06a9cab60ec7
Tatiana Fernandez <tferna03@nyit.edu>
2025-12-01 01:58:14 -0500

    Styled Forgot Password page

--- riw/src/pages/ForgotPassword.jsx
+++ riw/src/pages/ForgotPassword.jsx
@@ -1,4 +1,35 @@
-export default function ForgotPassword(){
-    //Code Here
-    
+import { Link } from "react-router-dom";
+import "../styles/forgotpassword.css";
+
+export default function ForgotPassword() {
+  return (
+    <div className="auth-page">
+      <div className="forgot-card">
+        <h1 className="forgot-title">
+          Forgot <br />
+          <span className="forgot-title-indent">Password</span>
+        </h1>
+
+        <form className="forgot-form">
+          <label className="forgot-label">
+            Email
+            <span className="forgot-note">
+              Enter email associated with this account
+            </span>
+
+            <input
+              type="email"
+              className="forgot-input"
+              placeholder="you@example.com"
+            />
+          </label>
+
+          <button className="forgot-btn">
+            Send Verification Code
+          </button>
+        </form>
+
+      </div>
+    </div>
+  );
 }
